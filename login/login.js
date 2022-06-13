ff1af9ba13ac8f5a2382f014f4280988fac7df21
Pererinha <daniboydiefenthaeler@gmail.com>
2022-06-13 00:16:56 -0300

    |salvando informações para validar o login|

--- login/login.js
+++ login/login.js
@@ -1,1 +1,10 @@
-console.log("Hello Word")
+const queryString = require('query-string');
+
+function efetuarLogin(login, password) {
+    loginBruto = document.querySelector("#loginInput")
+    login = loginBruto.value
+    console.log(login)
+    passwordBruto = document.querySelector("#passwordInput")
+    password = passwordBruto.value
+    console.log(password)
+};
